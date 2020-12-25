1d7c2ddc6ced77919ceb359fbcc36149df784a09
FilipeNespoli <filipenespoli@gmail.com>
2020-12-25 15:35:59 -0300

    primeiro commit

---
+++ netflixClone/js/main.js
@@ -0,0 +1,7 @@
+function assistir(){
+    document.getElementById("ver-filme").innerHTML = window.location.href = "player.html" 
+}
+
+function informar(){
+    document.getElementById("infornacao").innerHTML = window.location.href = "sinopses.html" 
+}
